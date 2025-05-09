1bbe28cb4d955693e12c1bbe7efe2e37831288ac
menna <twmnh195@gmail.com>
2025-05-09 14:07:57 +0300

    re 3

--- solutions/raindrops.sh
+++ solutions/raindrops.sh
@@ -20,3 +20,13 @@ fi
 if (( flag == 1 )); then
     echo $1
 fi
+
+
+# --------------------------------
+#       more clever solution
+
+(( $1 % 3 )) || out+="Pling" 
+(( $1 % 5 )) || out+="Plang" 
+(( $1 % 7 )) || out+="Plong" 
+
+echo ${out:-$1}     # bash handles it gracefully if `out` is unset
